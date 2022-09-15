fce5540c98b1a07a998f4141e52138eacbd55d57
emilyaikens <emily.c.aikens@gmail.com>
2022-09-15 10:18:06 -0700

    burrito

--- merge.py
+++ merge.py
@@ -1,6 +1,11 @@
+from email.utils import parsedate_to_datetime
+
+
 def emilys_function():
     print('this is emily testing stuff out')
 
 
 def pat_testing():
     pass
+
+lunch = 'i want a burrito'
